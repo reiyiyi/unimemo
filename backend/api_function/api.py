f04b4f9f53c0066f8ec3e90bf0cd331394e87680
reiyiyi <necro.2048@gmail.com>
2023-09-29 19:29:50 +0900

    バックエンド処理を行なうAPIファイルの追加

---
+++ backend/api_function/api.py
@@ -0,0 +1,144 @@
+import os
+import json
+import boto3
+from signup import SignupAPI
+from login import LoginAPI
+from searchChart import SearchChartAPI
+from searchTune import SearchTuneAPI
+from searchMemo import SearchMemoAPI
+from searchStatus import SearchStatusAPI
+from searchMirror import SearchMirrorAPI
+from changeInformation import ChangeInformationAPI
+from changeName import ChangeNameAPI
+from logout import LogoutAPI
+
+TABLE_NAME = os.getenv('TABLE_NAME')
+SESSION_INDEX_NAME = os.getenv('SESSION_INDEX_NAME')
+SEARCH_INDEX_NAME = os.getenv('SEARCH_INDEX_NAME')
+LEVEL_INDEX_NAME = os.getenv('LEVEL_INDEX_NAME')
+STATUS_INDEX_NAME = os.getenv('STATUS_INDEX_NAME')
+MEMO_INDEX_NAME = os.getenv('MEMO_INDEX_NAME')
+MIRROR_INDEX_NAME = os.getenv('MIRROR_INDEX_NAME')
+dynamodb = boto3.client('dynamodb')
+
+DB_ERROR_STATUS = "#DBerror"
+
+# セッション情報からユーザIDの取得をする関数
+def get_user_id(session):
+    # セッション情報が無い場合は空文字列を返す
+    if not session:
+        return ""
+    
+    try:
+        response = dynamodb.query(
+            TableName=TABLE_NAME,
+            IndexName = SESSION_INDEX_NAME,
+            KeyConditionExpression = "#se = :session_val",
+            ExpressionAttributeNames= {
+            '#se' : 'session',
+            },
+            ExpressionAttributeValues={":session_val": {"S": session}},
+        )
+    # エラーが発生した場合はエラーステータスを返す
+    except BaseException as be:
+        return DB_ERROR_STATUS
+    
+    # 与えられたセッション値と紐づけられているユーザIDが無い場合は空文字列を返す
+    if not response["Items"]:
+        return ""
+    
+    # ユーザIDを返す
+    return response["Items"][0]["id"]["S"]
+
+
+def handler(event, context):
+    request_body = eval(event["body"])
+    request_api_name = request_body["API"]
+    
+    # 新規登録処理を行なうAPI
+    if request_api_name == "SignupAPI":
+        return SignupAPI(request_body)
+    
+    # ログイン処理を行なうAPI
+    if request_api_name == "LoginAPI":
+        return LoginAPI(request_body)
+    
+    #--------以下のAPIはログイン状態であることが必要--------
+    session = request_body["session"]
+    user_id = get_user_id(session)
+    
+    # ユーザIDが取得出来なかった場合
+    if not user_id:
+        return {
+            'statusCode': 400,
+            'headers': {
+                "Access-Control-Allow-Headers": "Content-Type",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "OPTIONS,POST",
+                #"Access-Control-Allow-Credentials": 'true'
+            },
+            'body': json.dumps({
+                "message" : "Please login."
+            })
+        }
+    # セッション情報照合時にデータベース側でエラーが発生した場合
+    if user_id == DB_ERROR_STATUS:
+        return {
+            'statusCode': 500,
+            'headers': {
+                "Access-Control-Allow-Headers": "Content-Type",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "OPTIONS,POST",
+                #"Access-Control-Allow-Credentials": 'true'
+            },
+            'body': json.dumps({
+                "message" : "Internal server error."
+            })
+        }
+        
+    # 譜面情報をもとに譜面の検索処理を行なうAPI
+    if request_api_name == "SearchChartAPI":
+        return SearchChartAPI(user_id, request_body)
+    
+    # 曲名をもとに譜面の検索処理を行なうAPI
+    if request_api_name == "SearchTuneAPI":
+        return SearchTuneAPI(user_id, request_body)
+    
+    # メモが設定された譜面の検索処理を行なうAPI
+    if request_api_name == "SearchMemoAPI":
+        return SearchMemoAPI(user_id, request_body)
+    
+    # 特定のステータスが設定された譜面の検索処理を行なうAPI
+    if request_api_name == "SearchStatusAPI":
+        return SearchStatusAPI(user_id, request_body)
+    
+    # ミラー情報が設定された譜面の検索処理を行なうAPI
+    if request_api_name == "SearchMirrorAPI":
+        return SearchMirrorAPI(user_id, request_body)
+    
+    # 譜面に設定された情報の変更処理を行なうAPI
+    if request_api_name == "ChangeInformationAPI":
+        return ChangeInformationAPI(user_id, request_body)
+    
+    # 名前の変更処理を行なうAPI
+    if request_api_name == "ChangeNameAPI":
+        return ChangeNameAPI(user_id, request_body)
+    
+    # ログアウト処理を行なうAPI
+    if request_api_name == "LogoutAPI":
+        return LogoutAPI(user_id, request_body)
+    
+    
+    # 想定されていないAPI名が渡された場合
+    return {
+        'statusCode': 400,
+        "headers": {
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "OPTIONS,POST",
+            #"Access-Control-Allow-Credentials": 'true'
+        },
+        'body': json.dumps({
+            "message":"f{request_api_name} does not exist."
+        })
+    }

0f5f3f4ebea858264f745429b057dd72fa336705
reiyiyi <necro.2048@gmail.com>
2023-10-26 23:46:51 +0900

    inquiry.jsの実装"

---
+++ src/components/inquiry.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Inquiry = () => {
+    return (
+        <div className="p-3">
+            <div className="container">
+                <h4 className="my-3">問い合わせについて</h4>
+                <div className="introduction-card">
+                    <p>
+                        問い合わせに関しては、本サービスの開発者(れぎ)の<a href="https://twitter.com/Re_gi_A">X(Twitter)</a>のDMまたはリプライにて受け付けさせていただきます。
+                    </p>
+                    <p>
+                        何卒、よろしくお願いいたします。
+                    </p>
+                </div>    
+            </div>
+        </div>
+    )
+};
+
+export default Inquiry;

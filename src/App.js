6a9148dc6d4e87c072a18de0a7f65ea89c8b7e79
reiyiyi <necro.2048@gmail.com>
2023-10-26 19:12:27 +0900

    App.jsの実装

--- src/App.js
+++ src/App.js
@@ -1,36 +1,46 @@
-import React from 'react';
-import { Routes, Route } from "react-router-dom";
+import React, { useState } from 'react';
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { ToastContainer } from 'react-toastify';
 import './App.css';
+import './common.css';
+import GetCookieData from './api_access/getCookieData';
 import Top from './components/top';
+import Home from './components/home';
 import Login from './components/login';
 import Signup from './components/signup';
-import SearchChart from './components/searchChart';
-import SearchTune from './components/searchTune';
-import SearchChartResult from './components/searchChartResult';
-import SearchTuneResult from './components/searchTuneResult';
-import Information from './components/information';
 import InformationChange from './components/informationChange';
 import NameChange from './components/nameChange';
-import SearchStatus from './components/searchStatus';
-import SearchMirror from './components/searchMirror';
-import SearchMemo from './components/searchMemo';
-import SearchStatusResult from './components/searchStatusResult';
-import SearchMirrorResult from './components/searchMirrorResult';
-import SearchMemoResult from './components/searchMemoResult';
 import Logout from './components/logout';
 import Navbar from './components/navbar';
 import Error from './components/error';
+import Donation from './components/donation';
+import Inquiry from './components/inquiry';
 
 const App = () => {
+    const cookie_data = GetCookieData();
+    const [isAuth, setIsAuth] = useState('session' in cookie_data);
+
     return (
         <div className="App">
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<Top />}></Route>
-                <Route path="/login" element={<Login />}></Route>
-                <Route path="/signup" element={<Signup />}></Route>
-                <Route path="/logout" element={<Logout />}></Route>
-            </Routes>
+            <BrowserRouter>
+                <Navbar isAuth={isAuth} />
+                <ToastContainer
+                    autoClose={5000}
+                    position="bottom-right"
+                />
+                <Routes>
+                    <Route path="/" element={<Top />}></Route>
+                    <Route path="/signup" element={<Signup />}></Route>
+                    <Route path="/login" element={<Login setIsAuth={setIsAuth} />}></Route>
+                    <Route path="/home" element={<Home />}></Route>
+                    <Route path="/change/information/:chart_id" element={<InformationChange />}></Route>
+                    <Route path="/change/name" element={<NameChange />}></Route>
+                    <Route path="/logout" element={<Logout setIsAuth={setIsAuth} />}></Route>
+                    <Route path="/error/:status_code" element={<Error setIsAuth={setIsAuth} />}></Route>
+                    <Route path="/guide/donation" element={<Donation />}></Route>
+                    <Route path="/guide/inquiry" element={<Inquiry />}></Route>
+                </Routes>
+            </BrowserRouter>
         </div>
     );
 };
